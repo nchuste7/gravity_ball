5446e5be65c3ccc44d6ddeab603f1d081fc80df6
nchuste7 <nds5@illinois.edu>
2018-04-17 16:32:49 -0500

    sample code for gravity implementation

--- app/src/main/java/com/example/topbr/gravity_ball/ball.java
+++ app/src/main/java/com/example/topbr/gravity_ball/ball.java
@@ -2,6 +2,9 @@ package com.example.topbr.gravity_ball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.hardware.SensorEvent;
+import android.graphics.Rect;
 
 public class ball extends AppCompatActivity {
 
@@ -10,4 +13,24 @@ public class ball extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ball);
     }
+    float[] gravity = new float[3];
+    float[] linear_acceleration = new float[3];
+    public void onSensorChanged(SensorEvent event)
+    {
+        // alpha is calculated as t / (t + dT)
+        // with t, the low-pass filter's time-constant
+        // and dT, the event delivery rate
+
+        final double alphaOne = .8;
+        final float alpha = (float) alphaOne;
+
+        gravity[0] = alpha * gravity[0] + (1 - alpha) * event.values[0];
+        gravity[1] = alpha * gravity[1] + (1 - alpha) * event.values[1];
+        gravity[2] = alpha * gravity[2] + (1 - alpha) * event.values[2];
+
+        linear_acceleration[0] = event.values[0] - gravity[0];
+        linear_acceleration[1] = event.values[1] - gravity[1];
+        linear_acceleration[2] = event.values[2] - gravity[2];
+    }
+
 }

3ec449d79792d47084681d7d3a35c3978feb9ac4
nchuste7 <nds5@illinois.edu>
2018-04-21 13:17:17 -0500

    Changes To plan

--- app/src/main/java/com/example/topbr/gravity_ball/ball.java
+++ app/src/main/java/com/example/topbr/gravity_ball/ball.java
@@ -2,9 +2,8 @@ package com.example.topbr.gravity_ball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Gravity;
-import android.hardware.SensorEvent;
-import android.graphics.Rect;
+import android.widget.ImageView;
+
 
 public class ball extends AppCompatActivity {
 
@@ -12,25 +11,8 @@ public class ball extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ball);
-    }
-    float[] gravity = new float[3];
-    float[] linear_acceleration = new float[3];
-    public void onSensorChanged(SensorEvent event)
-    {
-        // alpha is calculated as t / (t + dT)
-        // with t, the low-pass filter's time-constant
-        // and dT, the event delivery rate
 
-        final double alphaOne = .8;
-        final float alpha = (float) alphaOne;
-
-        gravity[0] = alpha * gravity[0] + (1 - alpha) * event.values[0];
-        gravity[1] = alpha * gravity[1] + (1 - alpha) * event.values[1];
-        gravity[2] = alpha * gravity[2] + (1 - alpha) * event.values[2];
-
-        linear_acceleration[0] = event.values[0] - gravity[0];
-        linear_acceleration[1] = event.values[1] - gravity[1];
-        linear_acceleration[2] = event.values[2] - gravity[2];
+        ImageView airplane = findViewById(R.id.character);
     }
 
 }
